bb4896d3780b98fd174a10a99fe6b2d9b4275b0e
Clifford Richardson <cmrallen@gmail.com>
2016-03-22 22:14:20 -0700

    adjusted walker

--- tracer.py
+++ tracer.py
@@ -2,37 +2,42 @@ import ast
 import sys
 import os as o
 
-aliases = {}
-
 def deco(func):
     def wrapper():
         print "ho"
     return wrapper
 
 class Walker(ast.NodeVisitor):
+    def __init__(self):
+        self.ctx = ''
+        self.aliases = {}
+        super(Walker, self).__init__()
+
+    def walk_file(self, f):
+        self.ctx = f
+        self.aliases = {}
+        with open(f) as fil:
+            self.visit(ast.parse(fil.read()))
+
     def visit_Import(self, node):
         for name in node.__dict__['names']:
             if name.asname:
-                aliases[name.asname] = name.name
-            else:
-                aliases[name.name] = name.name
+                self.aliases[name.asname] = name.name
             ast.NodeVisitor.generic_visit(self, node)
-    def visit_ImportFrom(self, node):
-        for name in node.__dict__['names']:
-            print name.__dict__
-        ast.NodeVisitor.generic_visit(self, node)
+
     def visit_Call(self, node):
         func = node.func
         name = self.get_alias(func)
         print name
         ast.NodeVisitor.generic_visit(self, node)
+
     def generic_visit(self, node):
         ast.NodeVisitor.generic_visit(self, node)
 
     def get_alias(self, node, postfix=''):
         if isinstance(node, ast.Name):
             name = node.id
-            name = aliases.get(name,name)
+            name = self.aliases.get(name,name)
             if postfix:
                 name+='.'+postfix
             return name
@@ -46,9 +51,5 @@ def test():
 
 if __name__ == "__main__":
     o.getcwd()
-    _root_node = None
     walker = Walker()
-    with open(sys.argv[1],'r') as f:
-        _root_node = ast.parse(f.read())
-
-    walker.visit(_root_node)
+    walker.walk_file(sys.argv[1])
